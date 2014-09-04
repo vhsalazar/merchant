9921c1b27fe709d9e13919c285f70eeffc73da05
Victor Hugo Salazar <xtealc@gmail.com>
2014-09-04 18:22:22 -0400

    Adding Eway Rapid v.30 api

--- billing/integrations/eway_au_integration.py
+++ billing/integrations/eway_au_integration.py
@@ -1,4 +1,5 @@
 from billing import Integration, get_gateway, IntegrationNotConfigured
+from billing.gateways.eway_gateway.rapidapi.rapid import RapidAPI
 from billing.forms.eway_au_forms import EwayAuForm
 from django.conf import settings
 from django.conf.urls import patterns, url
@@ -148,3 +149,127 @@ class EwayAuIntegration(Integration):
 
         response = client.service.GetAccessCodeResult(req)
         return translate(dict(((k, getattr(response, k)) for k in dir(response))))
+
+
+
+
+
+
+
+class EwayRapidAuIntegration(Integration):
+    display_name = "eWAY"
+    service_url = "https://au.ewaygateway.com/mh/payment"
+    template = "billing/eway_api.html"
+    urls = ()
+
+    def __init__(self, access_code=None):
+        super(EwayRapidAuIntegration, self).__init__()
+        merchant_settings = getattr(settings, "MERCHANT_SETTINGS")
+        if not merchant_settings or not merchant_settings.get("eway_rapid"):
+            raise IntegrationNotConfigured("The '%s' integration is not correctly "
+                                           "configured." % self.display_name)
+        eway_settings = merchant_settings["eway_rapid"]
+        self.customer_id = eway_settings["CUSTOMER_ID"]
+        self.username = eway_settings["USERNAME"]
+        self.password = eway_settings["PASSWORD"]
+        # Don't use X-Forwarded-For. It doesn't really matter if REMOTE_ADDR
+        # isn't their *real* IP, we're only interested in what IP they're going
+        # to use for their POST request to eWAY. If they're using a proxy to
+        # connect to us, it's fair to assume they'll use the same proxy to
+        # connect to eWAY.
+        self.access_code = access_code
+
+    def generate_form(self):
+        initial_data = dict(EWAY_ACCESSCODE=self.access_code, **self.fields)
+        return EwayAuForm(initial=initial_data)
+
+    def request_access_code(self, payment, return_url, customer=None,
+                            billing_country=None, ip_address=None):
+        # # enforce required fields
+        # assert self.customer_id
+        # assert self.username
+        # assert self.password
+        # assert payment['total_amount']
+        # assert return_url
+        #
+        # # Request a new access code.
+        # req = client.factory.create("CreateAccessCodeRequest")
+        # req.Authentication.CustomerID = self.customer_id
+        # req.Authentication.Username = self.username
+        # req.Authentication.Password = self.password
+        # attr_update(req.Payment, translate(payment))
+        # attr_update(req.Customer, translate(customer or {}))
+        # req.RedirectUrl = return_url
+        # if ip_address:
+        #     req.IPAddress = ip_address
+        # if billing_country:
+        #     req.BillingCountry = billing_country
+        # del req.ResponseMode
+        #
+        # # Handle the response
+        # response = client.service.CreateAccessCode(req)
+        # self.access_code = response.AccessCode
+        #
+        # # turn customer to dict
+        # customer_echo = dict(((k, getattr(response.Customer, k))
+        #                       for k in dir(response.Customer)))
+        rapid = RapidAPI(api_method='REST', api_format='JSON', username=self.username, password=self.password, debug=True)
+        req = {
+            'Customer': customer,
+            'ShippingAddress': {},
+            'Options': [],
+            'Items': [],
+            'Payment': {
+                'TotalAmount': payment['total_amount'] * 100,
+                'InvoiceNumber': payment['order_id'],
+                'InvoiceDescription': '',
+                'InvoiceReference': '',
+                'CurrencyCode': 'AUD',
+            },
+            # Url to the page for getting the result with an AccessCode
+            # Note: RedirectUrl is a Required Field For all cases
+            'RedirectUrl': return_url,
+                'Method': 'TokenPayment', #data.get('ddlMethod'),
+            'CustomerIP': ip_address or ''
+
+        }
+        result = rapid.create_access_code(req)[0]
+        self.access_code = result['AccessCode']
+        self.service_url = result['FormActionURL']
+        # return (self.access_code, translate(customer_echo))
+        # customer_echo = dict(((k, getattr(result['Customer'], k))
+        #                               for k in dir(response.Customer)))
+        #translate(result['Customer'])
+        return (self.access_code, result)
+
+
+    def check_transaction(self):
+        if not self.access_code:
+            raise ValueError("`access_code` must be specified")
+
+        # req = client.factory.create("GetAccessCodeResultRequest")
+        # req.Authentication.CustomerID = self.customer_id
+        # req.Authentication.Username = self.username
+        # req.Authentication.Password = self.password
+        # req.AccessCode = self.access_code
+        #
+        # response = client.service.GetAccessCodeResult(req)
+
+        rapid = RapidAPI(api_method='REST', api_format='JSON', username=self.username, password=self.password, debug=True)
+        result = rapid.get_access_code({'AccessCode': self.access_code})[0]
+        options = result.get('Options')
+        data = {
+            'AccessCode': self.access_code,
+            'AuthorisationCode': result.get('AuthorisationCode'),
+            'InvoiceNumber': result.get('InvoiceNumber'),
+            'InvoiceReference': result.get('InvoiceReference'),
+            'ResponseCode': result.get('ResponseCode'),
+            'ResponseMessage': result.get('ResponseMessage'),
+            'TokenCustomerID': result.get('TokenCustomerID'),
+            'TotalAmount': result.get('TotalAmount'),
+            'TransactionID': result.get('TransactionID'),
+            'TransactionStatus': result.get('TransactionStatus'),
+            'BeagleScore': result.get('BeagleScore'),
+        }
+        return translate(data)
+        # return translate(dict(((k, getattr(response, k)) for k in dir(response))))
